39ad48c4b7e79f750535672506b1259a4e40686a
Ngoc Quy Do <dongocquyonline@gmail.com>
2020-12-25 11:13:38 +0700

    Add project files.

---
+++ QuanLyKho_17Dh110194.Module.Win/WinModule.Designer.cs
@@ -0,0 +1,35 @@
+﻿namespace QuanLyKho_17Dh110194.Module.Win {
+    partial class QuanLyKho_17Dh110194WindowsFormsModule {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            // 
+            // QuanLyKho_17Dh110194WindowsFormsModule
+            // 
+            this.RequiredModuleTypes.Add(typeof(QuanLyKho_17Dh110194.Module.QuanLyKho_17Dh110194Module));
+            this.RequiredModuleTypes.Add(typeof(DevExpress.ExpressApp.Win.SystemModule.SystemWindowsFormsModule));
+        }
+
+        #endregion
+    }
+}
---
+++ QuanLyKho_17Dh110194.Win/WinApplication.Designer.cs
@@ -0,0 +1,55 @@
+﻿namespace QuanLyKho_17Dh110194.Win {
+    partial class QuanLyKho_17Dh110194WindowsFormsApplication {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.module1 = new DevExpress.ExpressApp.SystemModule.SystemModule();
+            this.module2 = new DevExpress.ExpressApp.Win.SystemModule.SystemWindowsFormsModule();
+            this.module3 = new QuanLyKho_17Dh110194.Module.QuanLyKho_17Dh110194Module();
+            this.module4 = new QuanLyKho_17Dh110194.Module.Win.QuanLyKho_17Dh110194WindowsFormsModule();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            // 
+            // QuanLyKho_17Dh110194WindowsFormsApplication
+            // 
+            this.ApplicationName = "QuanLyKho_17Dh110194";
+            this.CheckCompatibilityType = DevExpress.ExpressApp.CheckCompatibilityType.DatabaseSchema;
+            this.Modules.Add(this.module1);
+            this.Modules.Add(this.module2);
+            this.Modules.Add(this.module3);
+            this.Modules.Add(this.module4);
+            this.UseOldTemplates = false;
+            this.DatabaseVersionMismatch += new System.EventHandler<DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs>(this.QuanLyKho_17Dh110194WindowsFormsApplication_DatabaseVersionMismatch);
+            this.CustomizeLanguagesList += new System.EventHandler<DevExpress.ExpressApp.CustomizeLanguagesListEventArgs>(this.QuanLyKho_17Dh110194WindowsFormsApplication_CustomizeLanguagesList);
+
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+
+        }
+
+        #endregion
+
+        private DevExpress.ExpressApp.SystemModule.SystemModule module1;
+        private DevExpress.ExpressApp.Win.SystemModule.SystemWindowsFormsModule module2;
+        private QuanLyKho_17Dh110194.Module.QuanLyKho_17Dh110194Module module3;
+        private QuanLyKho_17Dh110194.Module.Win.QuanLyKho_17Dh110194WindowsFormsModule module4;
+    }
+}
